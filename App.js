ca97bc36865da18174d555dc3c5cfa2cced625eb
Tilted zqh7y❤️‍🔥 <113249259+zqh7y@users.noreply.github.com>
2024-10-19 01:21:59 +0300

    2nd part

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Main from './src/screens/Main';
 import Create from './src/screens/Create';
+import Confirm from './src/screens/Confirm';
 import { useFonts } from 'expo-font';
 
 const Stack = createNativeStackNavigator();
@@ -23,7 +24,8 @@ export default function App({ navigation }) {
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Main" component={Main} navigation={navigation} />
-        <Stack.Screen name="Create" component={Create} />
+        <Stack.Screen name="Create" component={Create} navigation={navigation} />
+        <Stack.Screen name="Confirm" component={Confirm} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
